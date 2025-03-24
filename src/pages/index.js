5aa5d70ac0fa4f4703fc53a33316a308e200cb56
hrithikscube <hrithik@scube.me>
2025-03-24 09:34:34 +0530

    gap card template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,113 +1,92 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+'use client'
+import React from 'react';
+import { toPng } from 'html-to-image';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const Home = () => {
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  const handleDownload = () => {
+    const card = document.getElementById('card-template');
 
-export default function Home() {
+    toPng(card, { pixelRatio: 2 }) // Increase pixelRatio for higher resolution
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = `card-template.png`;
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.error('Failed to download image:', err);
+      });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='flex flex-col items-center justify-center lg:gap-6 gap-4 w-full h-screen lg:px-0 md:px-6 px-4'>
+
+      <div id="card-template" className='lg:w-[460px] h-80 w-full mx-auto bg-[#121212] flex flex-col lg:gap-4 gap-2 lg:p-6 p-4'>
+
+        <div className='flex items-start justify-between w-full'>
+
+          <h1 className='font-serif lg:text-3xl text-2xl w-3/12 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#D0A847] via-[#FCF6BA] to-[#B58727]'>GAP INC.</h1>
+
+          <img
+            src="/qr.svg"
+            alt="qr"
+            className='w-24 h-24'
+          />
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className='flex flex-col'>
+
+          <h2 className='uppercase lg:text-xl md:text-lg text-base font-semibold tracking-wide font-serif bg-clip-text text-transparent bg-gradient-to-r from-[#D0A847] via-[#FCF6BA] to-[#B58727]'>Kumar Gourav</h2>
+
+          <p className='font-sans lg:text-base md:text-sm text-xs bg-clip-text text-transparent bg-gradient-to-r from-[#D0A847] via-[#FCF6BA] to-[#B58727]'>Lead Support</p>
+
+        </div>
+
+        <div className='flex flex-col gap-2'>
+
+          <div className='flex items-center gap-1'>
+
+            <img src="pin.svg" alt="pin" className='w-5 h-5' />
+
+            <p className='text-sm font-sans bg-clip-text text-transparent bg-gradient-to-r from-[#D0A847] via-[#FCF6BA] to-[#B58727]'>Delhi, India</p>
+
+          </div>
+
+          <div className='flex items-center gap-1'>
+
+            <img src="whatsapp.svg" alt="whatsapp" className='w-5 h-5' />
+
+            <p className='text-sm font-sans bg-clip-text text-transparent bg-gradient-to-r from-[#D0A847] via-[#FCF6BA] to-[#B58727]'>+91 9731816547</p>
+
+          </div>
+
+          <div className='flex items-center gap-1'>
+
+            <img src="mail.svg" alt="mail" className='w-5 h-5' />
+
+            <p className='text-sm font-sans bg-clip-text text-transparent bg-gradient-to-r from-[#D0A847] via-[#FCF6BA] to-[#B58727]'>kumar@gap.com</p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='flex items-center justify-center lg:gap-4 gap-2 w-44 mx-auto'>
+
+        <button onClick={handleDownload} className='bg-blue-600 text-white lg:text-base text-sm py-3 px-6 cursor-pointer'>
+          Download
+        </button>
+
+      </div>
+
+
+
     </div>
-  );
+  )
 }
+
+export default Home
+
